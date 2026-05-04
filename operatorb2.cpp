f90f8e02937bc721b1ef33c91fb0794e568f01ef
quyphandinh25-glitch <quyphandinh25@gmail.com>
2026-05-04 15:04:13 +0700

    Update operatorb2.cpp

--- operatorb2.cpp
+++ operatorb2.cpp
@@ -1,8 +1,9 @@
-#include <bits/stdc++.h>
+
 /*2. Làm lại bài phân số với các phương thức thiết lập cho phép sử dụng một số nguyên
 như một phân số đặc biệt (mẫu số bằng 1). Định nghĩa các phép toán +, -, *, /, ==,
 >, < trên phân số. Định nghĩa phép toán << và >> để xuất và nhập dữ liệu cho phân
 số.*/
+#include <bits/stdc++.h>
 using namespace std;
 int ucln(int a, int b)
 {
