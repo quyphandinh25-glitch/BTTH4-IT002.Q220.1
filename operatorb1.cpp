67dc0e908486f99f8ee8edf63ecde0e046393665
quyphandinh25-glitch <quyphandinh25@gmail.com>
2026-05-04 15:03:55 +0700

    Update operatorb1.cpp

--- operatorb1.cpp
+++ operatorb1.cpp
@@ -1,7 +1,7 @@
-/*2. Làm lại bài phân số với các phương thức thiết lập cho phép sử dụng một số nguyên
-như một phân số đặc biệt (mẫu số bằng 1). Định nghĩa các phép toán +, -, *, /, ==,
->, < trên phân số. Định nghĩa phép toán << và >> để xuất và nhập dữ liệu cho phân
-số.*/
+/*1. Làm lại bài số phức với một phương thức thiết lập duy nhất cho phép quan điểm một
+số thực như một số phức đặc biệt (phần ảo bằng 0). Định nghĩa các phép toán +, -,
+*, /, ==, != trên số phức. Định nghĩa phép toán << và >> để xuất và nhập dữ liệu cho
+số phức.*/
 #include <iostream>
 using namespace std;
 
